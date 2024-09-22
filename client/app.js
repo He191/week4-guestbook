5870037809e65d43e91ae6d407b97a848979ec07
Hetal Patel <YOUR EMAIL ADDRESS>
2024-09-22 02:26:22 +0100

    Fixed code to resolve build error on render

--- client/app.js
+++ client/app.js
@@ -1,29 +1,22 @@
 // store the shop Upgrade from the API
 async function getFeedbackFromDB(){
-    const response = await fetch ("https://week4-guestbook.onrender.com");
+    const response = await fetch ("https://week4-guestbook.onrender.com:8080/");
+    // const response = await fetch ("http://localhost:8080/");
 // I need my data to be written in json.
     const data = await response.json();
     return data;
-}
-
-const fb = await getFeedbackFromDB();
-
-fb.forEach(function(a,i) {
-    const feedbackContainer = document.querySelector("#innerFB");
-    const feedback =document.createElement("div");
-    feedback.className="indFB";
-    feedback.textContent= fb[i].name +" From: "+fb[i].city+" " +fb[i].feedback;
-    feedbackContainer.appendChild(feedback);   
-});
-
-const form = document.querySelector("#form-container");
+};
 
 function handleSubmit(event) {
+    
     event.preventDefault(); // Prevent form submission
+    
+    const form = document.querySelector("#form-container");
+
     const formData = new FormData(form);
     const formValues = Object.fromEntries(formData);
-    // console.table(formValues);
-    fetch("https://week4-guestbook.onrender.com/add", {
+    fetch("https://week4-guestbook.onrender.com:8080/add", {
+    // fetch("http://localhost:8080/add", {
         method: "POST", // This is where we set the POST HTTP verb
         headers: {
           "Content-Type": "application/json", // This tells the server we're sending stringified JSON data
@@ -33,8 +26,20 @@ function handleSubmit(event) {
       form.reset();
 };
 
+async function main(){
 
-form.addEventListener("submit", handleSubmit);
+    const fb = await getFeedbackFromDB();
 
+    fb.forEach(function(a,i) {
+        const feedbackContainer = document.querySelector("#innerFB");
+        const feedback =document.createElement("div");
+        feedback.className="indFB";
+        feedback.textContent= fb[i].name +" From: "+fb[i].city+" " +fb[i].feedback;
+        feedbackContainer.appendChild(feedback);   
+    });
 
+    document.querySelector("#form-container").addEventListener("submit", handleSubmit);
 
+}
+
+main();
